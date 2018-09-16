752d220b20c9255492e4da2332bac816aed4ce86
unovikash <vikashkumar@unomaha.edu>
2018-09-15 23:47:51 -0500

    working model except service and product lists

--- crm/views.py
+++ crm/views.py
@@ -1,12 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 from .forms import *
 
+now = timezone.now()
+
+def home(request):
+    return render(request, 'crm/home.html', {'crm' : home}) #Third argument is optional
+
 # Create your views here.
+@login_required
 def customer_list(request):
     customer = Customer.objects.filter(created_date__lte=timezone.now())
     return render(request, 'crm/customer_list.html', {'customers': customer})
 
+@login_required
 def customer_edit(request, pk):
     customer = get_object_or_404(Customer, pk=pk)
     if request.method=="POST":
@@ -23,6 +31,7 @@ def customer_edit(request, pk):
         form = CustomerForm(instance=customer)
     return render(request, 'crm/customer_edit.html', {'form': form})
 
+@login_required
 def customer_delete(request, pk):
     customer = get_object_or_404(Customer, pk=pk)
     customer.delete()
--- mfscrm/urls.py
+++ mfscrm/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, include, re_path
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('crm.urls')),
+    #re_path(r'^accounts/login/$', views.login, name='login'),
+    #re_path(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page':'/'}),
+    re_path(r'^accounts/login/$', LoginView.as_view(template_name='registration/login.html'), name="login"),
+    re_path(r'^accounts/logout/$', LogoutView.as_view(), LogoutView.next_page, name="logout"),
 ]
